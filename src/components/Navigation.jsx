c4a49b52bebae81be4bdae72651c1304d0528650
allyroo <allyse.arucan@gmail.com>
2021-10-05 23:01:13 -0700

    first commit - organizational structure

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Navigation(props) {
+    return (
+        <div className="navigation">
+            
+        </div>
+    )
+}
