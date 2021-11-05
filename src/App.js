729d328355ebdc019666c18d124821d3db1395c8
allyroo <allyse.arucan@gmail.com>
2021-11-04 19:06:25 -0700

    mute rrd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+// import { BrowserRouter, Route } from "react-router-dom";
 import "./index.css";
 // import About from "./components/About";
 // import SignUp from "./components/Auth/SignUp";
