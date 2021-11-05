e2a6ee1591a9b6d7f66770a419e30c56aeba175f
allyroo <allyse.arucan@gmail.com>
2021-11-04 19:03:36 -0700

    dash/content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./index.css";
-import About from "./components/About";
-import SignUp from "./components/Auth/SignUp";
-import Login from "./components/Auth/Login";
+// import About from "./components/About";
+// import SignUp from "./components/Auth/SignUp";
+// import Login from "./components/Auth/Login";
 import ContentFrame from "./components/ContentFrame";
 import DashGrid from "./components/DashGrid";
 
